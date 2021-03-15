7c5dc9d619bb4a90a4750605d05fad075736af27
primaayunda <primariliayunda@gmail.com>
2021-03-16 00:49:51 +0700

    Done:)

--- Soal-[1].py
+++ Soal-[1].py
@@ -1,4 +1,4 @@
 nama = str(input("Masukkan nama: "))
 umur = int(input("Masukkan umur:  "))
 tinggi = float(input("Masukkan tinggi: "))
-print("Nama saya {}, umur saya {} tahun dan tinggi saya {} cm".format(nama, umur, tinggi))
+print("Nama saya {}, umur saya {} tahun, dan tinggi saya {}cm".format(nama, umur, tinggi))
--- Soal-[3].py
+++ Soal-[3].py
@@ -1,11 +1,11 @@
-# ujian_teori = str(input("Masukkan nilai ujian teori: "))
-# ujian_praktek = str(input("Masukkan nilai ujian praktek:  "))
+ujian_teori = int(input("Masukkan nilai ujian teori: "))
+ujian_praktek = int(input("Masukkan nilai ujian praktek:  "))
 
-# if ujian_teori or ujian_praktek >= "70":
-#   print ("Selamat, anda lulus!")
-# elif ujian_teori >= "70" or ujian_praktek < "70":
-#   print ("Anda harus mengulang ujian praktek.")
-# elif ujian_teori < "70" or ujian_praktek >= "70":
-#   print ("Anda harus mengulang ujian teori.")
-# else:
-#   print ("Anda harus mengulang ujian teori dan praktek")
+if ujian_teori >= 70 and ujian_praktek >= 70:
+  print ("Selamat, anda lulus!")
+elif ujian_teori >= 70 and ujian_praktek < 70:
+  print ("Anda harus mengulang ujian praktek.")
+elif ujian_teori < 70 and ujian_praktek >= 70:
+  print ("Anda harus mengulang ujian teori.")
+else:
+  print ("Anda harus mengulang ujian teori dan praktek.")
